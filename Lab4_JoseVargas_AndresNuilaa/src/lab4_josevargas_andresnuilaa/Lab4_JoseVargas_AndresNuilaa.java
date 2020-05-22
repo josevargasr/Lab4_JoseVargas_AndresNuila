9a55da8d42461f0f1851fa39e6741d6f975a5ce5
Jose Vargas <jose.vargasr@unitec.edu>
2020-05-22 13:14:38 -0600

    File del Proyecto

---
+++ Lab4_JoseVargas_AndresNuilaa/src/lab4_josevargas_andresnuilaa/Lab4_JoseVargas_AndresNuilaa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_josevargas_andresnuilaa;
+
+/**
+ *
+ * @author josevargas
+ */
+public class Lab4_JoseVargas_AndresNuilaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
